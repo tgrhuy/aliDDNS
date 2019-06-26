1f0225271e620803fcaa82d780921d4965ec402d
tgrhuy <tgrhuy@gmail.com>
2019-06-26 13:46:08 +0800

    Update DDNS_settings.py
    注意域名都为二级域名，如果是一级请使用['@.ccc.com']

--- DDNS_settings.py
+++ DDNS_settings.py
@@ -2,4 +2,4 @@ ALIACS = {"accessKeyId": "",
           "accessSecret": "",
           "zone": "cn-hangzhou"}
 IPURL = r'http://200019.ip138.com/'
-DOMAINNAME = ["",""]
+DOMAINNAME = ["www.ccc.com","fff.ccc.com"]
